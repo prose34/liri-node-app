76a609a4305a2843a71691151082616ec532f49f
Paul Rose <prose2334@gmail.com>
2018-05-02 23:17:31 -0500

    start omdb

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
+// import required keys and node packages
+
 require("dotenv").config();
 
 var importKeys = require("./keys.js")
-
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
@@ -10,55 +11,165 @@ var client = new Twitter(importKeys.twitter);
 var spotify = new Spotify(importKeys.spotify);
 
 
-// var params = {screen_name: 'CodingHW'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log(JSON.stringify(tweets, null, 2));
+// set up acceptance for whatever is entered into the command line. Join multiple words after the command is given.
 
-//   }
-// });
+var commandLineInput = process.argv;
+var command = process.argv[2];
+var userInput = '';
 
-
-var spotifyInput = process.argv;
-var trackName = '';
-
-for (var i = 2; i < spotifyInput.length; i++) {
-  trackName += ' ' + spotifyInput[i];
+for (var i = 3; i < commandLineInput.length; i++) {
+    // if (i > 3 && i < commandLineInput.length) {
+    //     userInput += userInput + "+" + commandLineInput[i];
+    // } else {
+        userInput += " " + commandLineInput[i];
+    // }
 }
 
-console.log(process.argv);
-console.log(trackName);
+// console.log(userInput);
+// console.log(commandLineInput);
+// run the function/command based on what command the user types
 
-spotify.search({ type: 'track', query: trackName, limit: 1}, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-  var spotifyOutput = JSON.stringify(data, null, 2);
-  console.log(spotifyOutput); 
+// if (commandLineInput[2] === "my-tweets") {
+//   runTwitter();
+// } else if (commandLineInput[2] === "spotify-this-song") {
+//   runSpotify();
+// } else {
+//   console.log("Please enter a command!");
+// };
 
-  // console.log()
-  // console.log("Song name: " + data.items[5].name);
+switch(command) {
+  case "my-tweets": 
+      runTwitter();
+      break;
 
-  // console.log("Artist(s): " + data.tracks.items[5].album.artists[0].name);
-  // console.log("Preview link: " + data.tracks.items[5].preview_url);
-  // console.log("Album: " + data.tracks.items[5].album.name);
-});
+  case "spotify-this-song":
+      if(userInput) { 
+        runSpotify(userInput);
+      } else {
+        runSpotify("The Sign Ace of Base");
+      }
+      break;
+
+  case "movie-this":
+      if(userInput) {
+         runOMDB(userInput);
+      } else {
+         runOMDB("Mr. Nobody");
+      }
+      break; 
+
+  case "do-what-it-says":
+      doThing();
+      break; 
+
+  default:
+      console.log("Please enter a command! Choose from the following: my-tweets, spotify-this-song, or movie-this");
+      break; 
+}
+
+// console.log(userInput);
+
+// Twitter function
+
+function runTwitter () {
+
+  var params = {screen_name: 'CodingHW'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(JSON.stringify(tweets, null, 2));
+
+    }
+  });
+
+};
 
 
 
 
-// node liri.js spotify-this-song '<song name here>'
+// Spotify function
 
-// This will show the following information about the song in your terminal/bash window
+function runSpotify (userInput) {
 
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
+  spotify.search({ type: 'track', query: userInput, limit: 5}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    console.log("========================================");
+
+    for(var i = 0; i < data.tracks.items.length; i++) {
+      var trackData = data.tracks.items[i];
+
+      // Artists Log
+      console.log("Artist(s): " + trackData.album.artists[0].name);
+
+      // Song Name
+      console.log("Song Name: " + trackData.name);
+
+      // Preview Link
+      console.log("Preview Link: " + trackData.preview_url);
+
+      // Ablum Title
+      console.log("Album Title: " + trackData.album.name);
+
+      // break between results
+      console.log("========================================");
+    }
+
+    var spotifyOutput = JSON.stringify(data, null, 2);
+    // console.log(spotifyOutput); 
+
+  });
+
+};
 
 
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
 
+function runOMDB (userInput) {
+
+  // Run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+
+  // This line is just to help us debug against the actual URL.
+  // console.log(queryUrl);
+
+  request(queryUrl, function(error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      // console.log("Release Year: " + JSON.parse(body).Year);
+
+      var movieBody = JSON.parse(body);
+      console.log(movieBody);
+
+      // Title of the movie
+      console.log("Movie Title: " + movieBody.Title);
+      // Release Year
+      console.log("Release Date: " + movieBody.Released)
+      //IMDB Rating of the movie.
+      console.log("IMDB Rating: " + movieBody.imdbRating);
+      //Rotten Tomatoes Rating of the movie.
+      // SOMETIMES MOVIES DON'T HAVE THIS!!!
+      console.log("Rotten Tomatoes Rating: " + movieBody.Ratings[1].Value);
+      //Country where the movie was produced.
+      console.log("Production Country: " + movieBody.Country);
+      // Language of the movie.
+      console.log("Language: " + movieBody.Language);
+      // Plot of the movie.
+      console.log("Plot: " + movieBody.Plot);
+      // Actors in the movie.
+      console.log("Actors: " + movieBody.Actors);
+
+
+    }
+  });
+
+
+
+}
 
 // ===============================================
 
@@ -77,14 +188,6 @@ spotify.search({ type: 'track', query: trackName, limit: 1}, function(err, data)
 
 
 // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-
-// It's on Netflix!
-
-// You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
-
 
 // ===============================================
 
